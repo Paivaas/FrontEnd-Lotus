588d083dc5a22eacbea4dd23dd886bfa3e1a2927
Julia Paiva <juliapoliver12@gmail.com>
2024-09-26 11:22:21 -0300

    :dizzy: feat: static screen

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import LotusIcon from "../../public/icons/utilities/lotus-icon.svg";
 import EmailIcon from "../../public/icons/profile-information/grey/email.svg"
 import KeyIcon from "../../public/icons/profile-information/grey/key.svg"
 import ArrowIcon from "../../public/icons/utilities/arrow-white.svg"
+import CircleDegrade from "../../public/icons/utilities/circle-degrade-2.svg"
 
 
 // Import dos componentes
@@ -21,14 +22,20 @@ export default function Home() {
 
   
   return (
-    <div className="flex w-screen h-screen max-xl:p-16 max-sm:p-8">
-
+    <div className="flex w-screen h-screen max-xl:p-16 max-sm:p-8 overflow-hidden">
       
-    <div className="h-full w-1/3 max-xl:w-0 max-xl:hidden">
+    <div className="h-full w-1/3 max-xl:w-0 max-xl:hidden relative bottom-16">
 
-      <div className="w-full h-1/3 bg-red-degrade-1 flex align-top justify-end"></div>
-      <div className="w-full h-1/3 bg-red-degrade-2"></div>
-      <div className="w-full h-1/3 bg-red-degrade-3"></div>
+      <div className="w-full h-1/3 flex items-start justify-start align-top">
+        <Image className="w-[500px] h-[500px] relative bottom-24 right-24" alt="Email Icon" src={CircleDegrade}></Image>
+      </div>
+      <div className="w-full h-1/3 flex items-center justify-end relative left-32 bottom-14">
+          <span className="w-[200px] h-[200px] animate-jump-in animate-infinite animate-duration-[6000ms] animate-alternate rounded-full relative bg-pink-3 opacity-40"></span>
+          <Image className="w-[200px] h-[200px] rounded-full relative right-[200px]" alt="Email Icon" src={CircleDegrade}></Image>
+      </div>
+      <div className="w-full h-1/3 flex justify-start items-center">
+        <Image className="w-[450px] h-[450px] rounded-full relative left-8 bottom-8" alt="Email Icon" src={CircleDegrade}></Image>
+      </div>
 
     </div>
 
@@ -40,7 +47,7 @@ export default function Home() {
           <Image className="w-[15%]" alt="Lotus Icon" src={LotusIcon}></Image>
 
           <h1 className="text-gray-3 text-5xl">Crie sua conta</h1>
-          <h2 className="text-gray-3">Ja possui uma conta? <span className="hover:text-gray-3 cursor-pointer transition duration-150 ease-in-out">Clique aqui</span> </h2>
+          <h2 className="text-gray-3">Ja possui uma conta? <span className="hover:text-pink-3 cursor-pointer transition duration-150 ease-in-out">Clique aqui</span> </h2>
         </div>
 
         {/* Campos para entrada de valor */}
@@ -100,13 +107,12 @@ export default function Home() {
 
           </div>
 
-          <button className="sw-1/3 transition duration-150 ease-in-out bg-gradient-to-r from-pink-3 to-orange-3 p-4 px-6 items-center justify-between rounded-full text-white flex hover:cursor-pointer hover:scale-95 max-sm:w-full max-sm:h-16">
+          <button className="sw-1/3 transition duration-150 ease-in-out bg-gradient-to-r from-pink-3 to-orange-3 p-4 px-6 items-center justify-between rounded-full text-white flex hover:cursor-pointer hover:scale-95 max-sm:w-full max-sm:h-16 ">
             
             <p className="text-xl">Próximo</p>
-            <Image className="w-[20%] max-sm:w-[5vw] " alt="Arrow Icon" src={ArrowIcon}></Image>
+            <Image className="w-[10%] max-sm:w-[5vw] " alt="Arrow Icon" src={ArrowIcon}></Image>
             
           </button>
-
           
         </div>
 
